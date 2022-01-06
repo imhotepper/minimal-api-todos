6f542516f9ad1a090cad390111215da3627065b9
dragos.stefanescu <dragos.stefanescu@ing.com>
2022-01-06 15:10:16 +0200

    Basic Authorization WIP - almost there

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,7 +14,10 @@ using Microsoft.OpenApi.Models;
 using System.Text.Json;
 using System.Threading.Tasks;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using MiniValidation;
 
 
@@ -22,11 +29,39 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = builder.Environment.ApplicationName, Version = "v1" });
+    c .AddSecurityDefinition("basic", new OpenApiSecurityScheme  
+    {  
+        Name = "Authorization",  
+        Type = SecuritySchemeType.Http,  
+        Scheme = "basic",  
+        In = ParameterLocation.Header,  
+        Description = "Basic Authorization header using the Bearer scheme."  
+    });  
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement  
+    {  
+        {  
+            new OpenApiSecurityScheme  
+            {  
+                Reference = new OpenApiReference  
+                {  
+                    Type = ReferenceType.SecurityScheme,  
+                    Id = "basic"  
+                }  
+            },  
+            new string[] {}  
+        }  
+    });
 });
 //add services used by the api
 builder.Services.AddScoped<TodosService>();
 // builder.Logging.AddJsonConsole();
 builder.Logging.AddConsole();
+
+
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -38,10 +73,23 @@ app.UseFileServer();
 
 //swagger initialization
 app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{builder.Environment.ApplicationName} v1"));
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{builder.Environment.ApplicationName} v1");
+     
+});
+
+//Add authentication
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 //GetAll
-app.MapGet("/api/todos", (TodosService todosService) => Results.Ok(todosService.GetAll()));
+app.MapGet("/api/todos", [Authorize] (TodosService todosService, ClaimsPrincipal user) =>
+{
+     var u = user;
+     return Results.Ok(todosService.GetAll());
+});//.RequireAuthorization();
 
 //GetById
 app.MapGet("/api/todos/{id}", (int id, TodosService todosService, ILogger<Todo> logger) =>
@@ -143,3 +191,66 @@ public class TodosService
 
     public Todo GetById(int id) => _todos.FirstOrDefault(x => x.Id == id);
 }
+
+
+  public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+       // private readonly IUserService _userService;
+
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock
+            //,IUserService userService
+            )
+            : base(options, logger, encoder, clock)
+        {
+          //  _userService = userService;
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            // skip authentication if endpoint has [AllowAnonymous] attribute
+            var endpoint = Context.GetEndpoint();
+            if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() != null)
+                return AuthenticateResult.NoResult();
+
+            
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+
+            User user= null;
+            
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+                var username = credentials[0];
+                var password = credentials[1];
+                user = new User(username);// await _userService.Authenticate(username, password);
+            }
+            catch
+            {
+                return AuthenticateResult.Fail("Invalid Authorization Header");
+            }
+
+            if (user == null)
+                return AuthenticateResult.Fail("Invalid Username or Password");
+
+            var claims = new[] {
+              //  new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+    }
+    
+    public record User(String Username);
+    
+    
